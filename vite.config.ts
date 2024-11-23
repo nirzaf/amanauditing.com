56885aafc40e8fa0c7f01e3e84735a138a98edc4
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-11-23 21:41:04 +0300

    vite configs added

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   optimizeDeps: {
     include: ['lucide-react']
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
   },
 });
